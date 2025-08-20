ce08fdf8662d417a162f2616c24e8ad77aaceef6
Clyde Howell <chowell@dfamilk.com>
2025-08-19 23:53:30 -0500

    Add VUE functionality for Artist page

---
+++ js/data.js
@@ -0,0 +1,19 @@
+var artists = [
+    {
+        name: "BeLinda Preston",
+        imageName: "Belinda Preston.png",
+        bio: "This new shining talent is lighting up the digital music arena with her sassy style and sultry twang. Her first release 'New Chapter - Who Dis?' became a viral sensation literally overnight.  It's safe to say we'll be hearing a lot more from BeLinda in the near future."
+    },
+    {
+        name: "Porter Dean",
+        imageName: "Porter Dean.png",
+        bio: "Porter Dean's bio goes here."
+    },
+    {
+        name: "Bayliana",
+        imageName: "Bayliana.png",
+        bio: "Bayliana's bio goes here."
+    }
+];
+
+
